9b4f57f415deb8cea76c2e56c01d6e8030b769d5
Taylor Yount <tayloryount@Taylors-MacBook-Air.local>
2018-07-18 00:08:35 -0400

    modified Pop Up styling

--- src/main.js
+++ src/main.js
@@ -5,17 +5,6 @@ import ReactDOM from 'react-dom';
 import Popup from './components/Popup'
 
 
-// let headerElement = (
-//   <h1>
-//     I am a React element! Hear me roar!
-//   </h1>
-// );
-//
-// ReactDOM.render(
-//   headerElement,
-//   document.getElementById('app')
-// );
-
 ReactDOM.render(
 	  <Popup artist="Taylor Swift" />,
 	  document.getElementById('ad')
